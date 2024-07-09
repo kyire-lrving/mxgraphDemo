8a332098685fa7e2918341afed0be1320ac1c3aa
wxt <1040863458@qq.com>
2024-07-09 17:34:49 +0800

    新增两个

--- App.vue
+++ App.vue
@@ -1,16 +1,46 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <el-tabs v-model="selectRoute" @tab-click="handleClick" type="border-card">
+      <el-tab-pane v-for="item in tabArr" :key="item.route" :label="item.name" :name="item.name"> </el-tab-pane>
+      <compontent :is="selectRoute"></compontent>
+    </el-tabs>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import demoFrist from '@/components/demoFrist'
+import demoConfig from '@/components/demoConfig'
+import mxHierarchicalLayout from '@/components/mxHierarchicalLayout'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    demoFrist,
+    demoConfig,
+    mxHierarchicalLayout
+  },
+  data() {
+    return {
+      selectRoute: 'demo-frist',
+      tabArr: [
+        {
+          name: '第一次小demo',
+          route: 'demo-frist'
+        },
+        {
+          name: 'mxgraph的自定义配置',
+          route: 'demo-config'
+        },
+        {
+          name: '绘制具备树形结构的拓扑图',
+          route: 'mxHierarchicalLayout'
+        }
+      ]
+    }
+  },
+  methods: {
+    handleClick(tab) {
+      this.selectRoute = this.tabArr[tab.index].route
+    }
   }
 }
 </script>
@@ -22,6 +52,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+@import './style/index.scss';
 </style>
--- components/demoFrist.vue
+++ components/demoFrist.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script>
-import { mxGraph } from 'mxgraph-js'
+import { mxGraph, mxRubberband } from 'mxgraph-js'
 
 export default {
   name: 'HelloWorld',
@@ -13,11 +13,17 @@ export default {
     var parent = graph.getDefaultParent()
     // 开始更新画布
     graph.getModel().beginUpdate()
+    var rubberBand = new mxRubberband(graph)
+    console.log(rubberBand)
     try {
-      // 插入节点
+      // 插入节点  /**
+      // mxGraph.insertVertex(parent, id, value, x, y, width, height, style)
+      // 在调用开始/结束更新中，创建并插入一个新的节点到模型中。
       let v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30)
       let v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30)
       // 插入连线
+      // mxGraph.insertEdge(parent, id, value, source, target, style)
+      // –在调用开始/结束更新中，创建并插入一条新的连线到模型中。
       graph.insertEdge(parent, null, '', v1, v2)
     } finally {
       // 画布更新结束
